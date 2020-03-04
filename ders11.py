de90cea4fbb34c858d77df00be5b704fd6d49983
Enes KARALİ <38677441+karalienes@users.noreply.github.com>
2020-03-04 04:01:05 +0300

    Update ders11.py

--- ders11.py
+++ ders11.py
@@ -8,4 +8,3 @@ while(a<20):
     print("a'nin degeri" , a)
     a=a+1
 """ a=1+a işlemi ile a +=1 işlemi aynı işlemi göstermektedir."""
-print("KARADENİZ TEKNİK ÜNİVERSİTESİ\n YÜK. BİLGİSAYAR MÜHENDİSİ ENES KARALİ")
