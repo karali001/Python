16568eddfcf8873289b344f27e321f6447c7409d
Enes KARALİ <38677441+karalienes@users.noreply.github.com>
2020-03-04 03:59:39 +0300

    Update ders10.py

--- ders10.py
+++ ders10.py
@@ -1,16 +1,15 @@
 """YÜK.BİLGİSAYAR MÜHENDİSİ ENES KARALİ BYS GİRİŞ SAYFASI"""
-defkullanıcı="karali001"
-defparalo="trabzonrize6153"
+defkullanıcı="abc"
+defparalo="xyz"
 
-kullanıcı = input("Enes bey kullanıcı adınızı giriniz:")
-paralo =input("Enes bey paralonızı giriniz:")
+kullanıcı = input("kullanıcı adınızı giriniz:")
+paralo =input("paralonızı giriniz:")
 
 if ((defparalo==paralo) and (defkullanıcı==kullanıcı)):
-    print("YÜK.BİLGİSAYAR MÜHENDİSİ Enes bey sisteme hoşgeldiniz.")
+    print("sisteme hoşgeldiniz.")
 elif((defparalo==paralo) and (defkullanıcı!=kullanıcı)):
-     print("Enes bey kullanıcı adınızı yanlış girdiniz!")
+     print("kullanıcı adınızı yanlış girdiniz!")
 elif((defparalo !=paralo) and (defkullanıcı==kullanıcı)):
-     print("Enes bey paralonuzu yanlıs gırdınız!")
+     print("paralonuzu yanlıs gırdınız!")
 else:
-     print("Hatalı giriş,Enes beye bildirileceksiniz.")
-print("KARADENİZ TEKNİK ÜNİVERSİTESİ\n YÜK. BİLGİSAYAR MÜHENDİSİ ENES KARALİ")
+     print("Hatalı giriş.")
